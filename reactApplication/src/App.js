a3589630aae568df93f66517ff3196350f8b0bcb
rehmanjaffar <142385806+rehmanjaffar@users.noreply.github.com>
2023-09-07 00:07:26 +0500

    Update App.js

--- reactApplication/src/App.js
+++ reactApplication/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World this is Avish Monga!</h1>
-      <p>this is my hello-world program implementing DOCKER</p>
+      <h1>Hello World this is Abdul Rehman!</h1>
+      <p>this is my hello-world program </p>
     </div>
   );
 }
