2f498e20f444a5413af6683a92ac8b153ec7de4a
rehmanjaffar <142385806+rehmanjaffar@users.noreply.github.com>
2023-09-06 23:50:41 +0500

    Add files via upload

---
+++ reactApplication/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World this is Avish Monga!</h1>
+      <p>this is my hello-world program implementing DOCKER</p>
+    </div>
+  );
+}
+
+export default App;
